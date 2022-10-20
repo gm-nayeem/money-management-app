a36d8c881c6220c2f20e216429eb272f576deaa3
golam mostafa <gmnayeem25@gmail.com>
2022-10-20 21:45:42 +0600

    add store

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {Provider} from 'react-redux'
+import store from './store'
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <div>
+  <Provider store={store}>
     <App />
-  </div>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
