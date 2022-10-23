2c2c9470870ca99605f9976b64655ef0ddd338ff
golam mostafa <gmnayeem25@gmail.com>
2022-10-23 14:34:05 +0600

    fix: dashboard

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,9 +1,46 @@
 import React, { Component } from 'react'
 
-export default class Dashboard extends Component {
+import {connect} from 'react-redux'
+import { loadTransactions } from '../store/actions/transactionAction'
+
+class Dashboard extends Component {
+
+    componentDidMount() {
+        this.props.loadTransactions()
+    }
+
   render() {
+    const {auth, transactions} = this.props
+    console.log(transactions);
     return (
-      <div>Dashboard</div>
+      <div className='row'>
+        <div className='col-md-8 offset-md-2'>
+            <h1>Welcome {auth.user.name}</h1>
+            <p>Your Email is {auth.user.email}</p>
+            <br/>
+            <h2>Transactions: </h2>
+            <ul className='list-group'>
+                {
+                    transactions.map(transaction => (
+                        <li 
+                            key={transaction._id}
+                            className='list-group-item'
+                        >
+                            <p>Type: {transaction.type}</p>
+                            <p>Amount: {transaction.amount}</p>
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+      </div>
     )
   }
 }
+
+const mapStateToProps = (state) => ({
+    auth: state.auth,
+    transactions: state.transactions
+})
+
+export default connect(mapStateToProps, {loadTransactions})(Dashboard);
